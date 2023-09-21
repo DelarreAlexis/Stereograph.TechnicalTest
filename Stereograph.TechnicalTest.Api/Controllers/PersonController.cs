edeaf87f5d687469f50c430f7702f8083c00bd7c
Alexis Delarre <delarre.alexis@gmail.com>
2023-09-21 19:23:19 +0200

    Ajout de la route DELETE

--- Stereograph.TechnicalTest.Api/Controllers/PersonController.cs
+++ Stereograph.TechnicalTest.Api/Controllers/PersonController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stereograph.TechnicalTest.Api.Models;
+using Stereograph.TechnicalTest.Api.Repository;
+using System.Collections.Generic;
 
 namespace Stereograph.TechnicalTest.Api.Controllers;
 
@@ -6,4 +9,39 @@ namespace Stereograph.TechnicalTest.Api.Controllers;
 [ApiController]
 public class PersonController : ControllerBase
 {
+    private readonly IPersonRepository _personRepository;
+
+    public PersonController(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<Person> persons = _personRepository.GetAll();
+        if(persons.Count == 0)
+            return NotFound();
+        return Ok(persons);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        Person person = _personRepository.Get(id);
+        if(person == null)
+            return NotFound();
+        return Ok(person);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        if (_personRepository.Delete(id))
+            return Ok();
+        return NotFound();
+    }
+
+
+
 }
--- Stereograph.TechnicalTest.Api/Repository/PersonRepository.cs
+++ Stereograph.TechnicalTest.Api/Repository/PersonRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Stereograph.TechnicalTest.Api.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Stereograph.TechnicalTest.Api.Repository
@@ -6,6 +7,9 @@ namespace Stereograph.TechnicalTest.Api.Repository
     public interface IPersonRepository
     {
         bool IsEmptyTable();
+        List<Person> GetAll();
+        Person Get(int id);
+        bool Delete(int id);
     }
 
     public class PersonRepository: IPersonRepository
@@ -15,6 +19,26 @@ namespace Stereograph.TechnicalTest.Api.Repository
         public PersonRepository(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        public List<Person> GetAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person Get(int id)
+        {
+            return _context.Persons.Where(person => person.Id == id).FirstOrDefault();
+        }
+
+        public bool Delete(int id)
+        {
+            Person person = Get(id);
+            if (person == null)
+                return false;
+            _context.Persons.Remove(person);
+            _context.SaveChanges();
+            return true;
         }
 
         public bool IsEmptyTable()
